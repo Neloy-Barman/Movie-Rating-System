55b26922b6f804ccf81e410276779d1819d18c90
Neloy Barman <neloycareer018@gmail.com>
2024-04-03 12:56:47 +0600

    Initialization notebook added

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import sys
+# To avoid generating pycache 
+sys.dont_write_bytecode = True
+
 from flask import Flask
 from bluePrints.user.user import user
 from bluePrints.movies.movies  import movies
 from bluePrints.ratings.ratings import ratings
 
-# To avoid generating pycache 
-sys.dont_write_bytecode = True
-
 app = Flask(__name__)
 
 app.secret_key = 'Movie_Rating_System'
-
 
 app.register_blueprint(user)
 
